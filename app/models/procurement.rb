9780de5197b164aa836c7c010ed0ffbe961833d9
gersonvan <gersonvan@gmail.com>
2016-06-09 23:29:05 -0300

    consertar search

--- app/models/procurement.rb
+++ app/models/procurement.rb
@@ -2,7 +2,6 @@ class Procurement < ActiveRecord::Base
   validates :cliente, presence: true, length: { minimum: 1 }
   
   def self.search(search)
-    where("cliente LIKE ?", "%#{search}%")
-    where("numero_do_processo LIKE ?", "%#{search}%") 
+    where("cliente LIKE ? or cast(numero_do_processo as text) LIKE ?", "%#{search}%", "%#{search}%")
   end
 end
