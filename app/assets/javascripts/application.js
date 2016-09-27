efc216b92eb7e9c868a9c679dbc803e78490d237
gersonvan <gersonvan@gmail.com>
2016-09-27 11:44:22 -0300

    Tentativa de corrigir a multipla selecao, sem sucesso

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,3 +34,14 @@ $(document).ready(function(){
 $(document).ready(function() {
   $('#saveButton').on('click', function() { $('#myForm').submit(); });
 });
+
+new Awesomplete('input[data-multiple]', {
+	filter: function(text, input) {
+		return Awesomplete.FILTER_CONTAINS(text, input.match(/[^,]*$/)[0]);
+	},
+
+	replace: function(text) {
+		var before = this.input.value.match(/^.+,\s*|/)[0];
+		this.input.value = before + text + ", ";
+	}
+});
