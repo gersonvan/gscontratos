db1a5a541e4222b6f3b95b0b3333665f2db7d993
gersonvan <gersonvan@gmail.com>
2016-11-29 15:55:05 -0300

    Mudada estrutura do PDF e adicionado limitação do campo Observações Complementares

--- app/models/procurement.rb
+++ app/models/procurement.rb
@@ -1,5 +1,6 @@
 class Procurement < ActiveRecord::Base
-  validates :cliente, presence: true, length: { minimum: 1 }
+  validates :cliente, presence: true, length: { minimum: 3 }
+  validates :observacoes_complementares , length: { maximum: 200 }
   
   def self.search(search)
     where("cliente LIKE ? or cast(numero_do_processo as text) LIKE ?", "%#{search}%", "%#{search}%")
